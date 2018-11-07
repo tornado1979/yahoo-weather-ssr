5e6209fd707fda08b6cd22a3f96281faf31b0ff7
Theodoros Itzaris <theodoreme@gmail.com>
2018-11-07 09:18:17 +0200

    Add createStore server side

---
+++ server/src/helpers/renderer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import { Provider }  from 'react-redux';
+import { renderRoutes } from 'react-router-config';
+
+import Routes from '../client/Routes';
+
+export default (req, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <div>{renderRoutes(Routes)}</div>
+      </StaticRouter>
+    </Provider>
+  );
+  return `
+  <html>
+   <head>Server Side render application</head>
+   <body>
+     <div id="root">${content}</div>
+     <script src="bundle.js"></script>
+   </body>
+  </html>
+  `;
+}
