c19f58d52596faefc14681138257dc978e64de2e
Theodoros Itzaris <theodoreme@gmail.com>
2018-11-07 11:31:21 +0200

    Add error handling on fetchData action

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -3,13 +3,17 @@ import { FETCH_DATA } from './types'
 
 export const fetchData = (city = 'San Diego') => async dispatch => {
   const API_URL = 'https://query.yahooapis.com/v1/public/yql?q='
-  const query = `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=${city})`
+  const query = `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}')`
   const format = '&format=json'
 
-  const res = await axios.get(`${API_URL}${query}${format}`)
+  try {
+    const res = await axios.get(`${API_URL}${query}${format}`)
 
-  dispatch({
-    payload: res,
-    type: FETCH_DATA,
-  })
+    dispatch({
+      payload: res,
+      type: FETCH_DATA,
+    })
+  } catch (err) {
+    console.log('ERROR: ', err) // eslint-disable-line
+  }
 }
