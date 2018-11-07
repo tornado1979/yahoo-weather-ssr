b38d02719c5ea6fb89913e227f010b6d0e792de5
Theodoros Itzaris <theodoreme@gmail.com>
2018-11-07 10:22:38 +0200

    Add Forecast component

---
+++ server/src/client/components/Forecast.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+export const Forecast = ({ forecast }) => {
+  return (
+    <div className="forecast-item">
+      <div className="day">{forecast.day}</div>
+      <div className="temp-container">
+        <div className="low-temp">
+          <span style={{ fontWeight: 600 }}>L</span>
+          {forecast.low}
+        </div>
+        <div className="high-temp">
+          <span style={{ fontWeight: 600 }}>H</span>
+          { forecast.high }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Forecast.propTypes = {
+  forecast: propTypes.shape({
+    day: propTypes.string,
+    hight: propTypes.string,
+    low: propTypes.string,
+  }).isRequired,
+}
